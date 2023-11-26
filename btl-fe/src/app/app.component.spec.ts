26795cacfa47bee937ae0862203c58a2c5199203
anhnt <tien226anh@gmail.com>
2023-11-26 23:17:21 +0700

    add angular

--- btl-fe/src/app/app.component.spec.ts
+++ btl-fe/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'btl_fe' title`, () => {
+  it(`should have as title 'btl-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('btl_fe');
+    expect(app.title).toEqual('btl-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, btl_fe');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, btl-fe');
   });
 });
